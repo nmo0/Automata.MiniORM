66495a7185c9d168df73acf30ecfcfe09e870c6a
Visten <xitong.wei@nio.com>
2018-02-28 11:57:11 +0800

    init project

---
+++ Automata.MiniORM/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata.MiniORM
+{
+    /// <summary>
+    /// 用于ORM的基类
+    /// </summary>
+    public class BaseModel
+    {
+    }
+}
